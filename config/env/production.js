6c6164f5daa7ad4503a5776bf7e165f9c26bf911
Hassan <hassan.reyes@gmail.com>
2015-10-30 16:55:52 -0600

    changed PORT to process.env.PORT

--- config/env/production.js
+++ config/env/production.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	db: process.env.MONGOLAB_URI || process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/checkit',
-	port: process.env.OPENSHIFT_NODEJS_PORT || PORT,
+	port: process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT,
 	assets: {
 		lib: {
 			css: [
