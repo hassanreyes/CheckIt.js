4150166219e40b1acb1bdc9bc9ee96324d7098be
Hassan <hassan.reyes@gmail.com>
2015-10-30 09:58:16 -0600

    client socket.io port changed to an static 8000

--- public/modules/core/services/socket.client.service.js
+++ public/modules/core/services/socket.client.service.js
@@ -2,7 +2,8 @@
 
 angular.module('checkit').factory('socket', function ($rootScope, $location, socketFactory) {
   
-    var url = $location.protocol() + '://' + $location.host() + ':' + $location.port();
+    //var url = $location.protocol() + '://' + $location.host() + ':' + $location.port();
+    var url = $location.protocol() + '://' + $location.host() + ':8000';
 
     var socket = io.connect(url + '/workingOn', {
       transports: [
