d3b73797ee510412796b3d6e7c015c019c6787c7
Hassan <hassan.reyes@gmail.com>
2015-11-27 17:35:58 -0600

    Bug fixed: $http reference missed

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication', '$http',
+	function($scope, Authentication, $http) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
 		$scope.addedChecklists = [];
