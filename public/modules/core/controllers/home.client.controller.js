458f1597e6becf78c03bffef4658ffbf72555859
Hassan <hassan.reyes@gmail.com>
2015-11-27 17:13:18 -0600

    LastAdded well panel added to home page

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,16 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+		$scope.addedChecklists = [];
+
+		$scope.init = function(){
+			$scope.getLastAdded();
+		};
+
+		$scope.getLastAdded = function(){
+			$http.get("dashboard/lastAdded", $scope.user).success(function(result){
+				$scope.addedChecklists = result;
+			});
+		};
 	}
 ]);
